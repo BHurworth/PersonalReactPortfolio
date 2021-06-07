c48994f0b9e0f0b42321fe71cdde74f62af248bd
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-07 22:23:44 +0100

    feat: initialise SideNav component

---
+++ src/components/Header/SideNav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SideNav( { show }) {
+    return (
+        <div className={ show ? 'side-nav-active' : 'side-nav-inactive'}>
+            
+        </div>
+    )
+}
+
+export default SideNav
