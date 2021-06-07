38a08474849dce8562be7314d25fb4e6304bb671
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-07 22:23:19 +0100

    feat: add state for sideNav bar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,33 @@
 import React, { useState } from "react";
-import Burger from "@animated-burgers/burger-squeeze"
-import "@animated-burgers/burger-squeeze/dist/styles.css"
-import logo from '../../assets/images/Group 1.png'
-
+import Burger from "@animated-burgers/burger-squeeze";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
+import logo from "../../assets/images/Group 1.png";
+import SideNav from "./SideNav.js";
 
 function Header() {
   const [active, setActive] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
   const burgerHandler = () => {
     setActive(!active);
+    setShowNav(!showNav);
   };
   return (
     <>
       <div className="nav-header">
         <ul className="nav-header-items">
           <img src={logo} id="logo"></img>
-          <Burger isOpen= { active} title="Squeeze" onClick={() => burgerHandler()} id="burger"></Burger>
+          <Burger
+            isOpen={active}
+            title="Squeeze"
+            onClick={() => burgerHandler()}
+            id="burger"
+          >
+            <div id="burger-lines"></div>
+          </Burger>
         </ul>
       </div>
+      <SideNav show={showNav} />
     </>
   );
 }
