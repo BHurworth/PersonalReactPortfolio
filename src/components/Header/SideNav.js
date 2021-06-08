fce10272386daa1581db356aaeefd170e9c2c129
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-08 11:52:16 +0100

    feat: add links in SideNav

--- src/components/Header/SideNav.js
+++ src/components/Header/SideNav.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import logo from "../../assets/images/logo-black.png";
 
 function SideNav( { show }) {
     return (
-        <div className={ show ? 'side-nav-active' : 'side-nav-inactive'}>
-            
-        </div>
-    )
+      <div className={show ? "side-nav-active" : "side-nav-inactive"}>
+        <ul className={show ? "side-nav-links-active" : 'side-nav-links-inactive'}>
+          <li><img src={logo} className='side-nav-logo'></img></li>
+          <li><div className='side-nav-divider'></div></li>
+          <li className="side-nav-link">Contact Me</li>
+          <li className="side-nav-link">About Me</li>
+          <li className="side-nav-link">My Photography</li>
+        </ul>
+      </div>
+    );
 }
 
 export default SideNav
