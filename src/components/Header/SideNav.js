6b52708abf6b852c53170944b28b9c0d65bc9b01
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-09 12:44:09 +0100

    feat: add NavLinks to sideNav links to create functionality

--- src/components/Header/SideNav.js
+++ src/components/Header/SideNav.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/images/logo-black.png";
 
-function SideNav( { show }) {
-    return (
-      <div className={show ? "side-nav-active" : "side-nav-inactive"}>
-        <ul className={show ? "side-nav-links-active" : 'side-nav-links-inactive'}>
-          <li><img src={logo} className='side-nav-logo'></img></li>
-          <li><div className='side-nav-divider'></div></li>
+function SideNav({ show }) {
+  return (
+    <div className={show ? "side-nav-active" : "side-nav-inactive"}>
+      <ul
+        className={show ? "side-nav-links-active" : "side-nav-links-inactive"}
+      >
+        <li>
+          <img src={logo} className="side-nav-logo"></img>
+        </li>
+        <li>
+          <div className="side-nav-divider"></div>
+        </li>
+
+
+        <NavLink to="/" exact activeClassName="active" className="Link">
+          <li className="side-nav-link">Home</li>
+        </NavLink>
+        <NavLink to='/contact-me' exact activeClassName="active" className="Link">
           <li className="side-nav-link">Contact Me</li>
+        </NavLink>
+        <NavLink to='/about-me' exact activeClassName="active" className="Link">
           <li className="side-nav-link">About Me</li>
+        </NavLink>
+        <NavLink to="/photography" exact activeClassName="active" className="Link">
           <li className="side-nav-link">My Photography</li>
-        </ul>
-      </div>
-    );
+        </NavLink>
+      </ul>
+    </div>
+  );
 }
 
-export default SideNav
+export default SideNav;
