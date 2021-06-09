beda2f645e6f71c1dc1023cb74e9827005f632db
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-09 12:45:16 +0100

    feat: initialise personalSkills.js

---
+++ src/components/pages/LandingPage/personalSkills/personalSkills.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function personalSkills() {
+    return (
+      <div className="personalSkills-wrapper">
+        <h1 id="projects-title">Personal Skills</h1>
+        <div id="divider-line"></div>
+      </div>
+    );
+}
