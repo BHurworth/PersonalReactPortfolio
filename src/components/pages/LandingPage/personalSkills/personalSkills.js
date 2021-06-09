f00c38e335f555f3c3e6e499dd98a5985e792803
Ben Hurworth <ben.hurworth@gmail.com>
2021-06-09 17:00:28 +0100

    feat: update blob image to new version

--- src/components/pages/LandingPage/personalSkills/personalSkills.js
+++ src/components/pages/LandingPage/personalSkills/personalSkills.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import blobFinal from '../../../../assets/images/blob-collection.png';
 
 export default function personalSkills() {
     return (
       <div className="personalSkills-wrapper">
-        <h1 id="projects-title">Personal Skills</h1>
+        <h1 id="sub-title">Personal Skills</h1>
         <div id="divider-line"></div>
+        <img src={blobFinal} className="personalSkills-blob"></img>
       </div>
     );
 }
